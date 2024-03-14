a3fc7f36272e9d00cfda24ad1f51403e874351bd
ioque <dev.arven@gmail.com>
2024-03-14 18:31:40 +0600

    рефакторинг ExchangeControllerTest

--- backend/adapters/src/test/java/ru/ioque/investfund/adapters/rest/ExchangeControllerTest.java
+++ backend/adapters/src/test/java/ru/ioque/investfund/adapters/rest/ExchangeControllerTest.java
@@ -5,13 +5,19 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import ru.ioque.investfund.adapters.rest.exchange.request.DisableUpdateInstrumentRequest;
+import ru.ioque.investfund.adapters.rest.exchange.request.EnableUpdateInstrumentRequest;
+import ru.ioque.investfund.adapters.rest.exchange.response.ExchangeResponse;
 import ru.ioque.investfund.adapters.rest.exchange.response.InstrumentInListResponse;
 import ru.ioque.investfund.adapters.rest.exchange.response.InstrumentResponse;
 import ru.ioque.investfund.adapters.storage.jpa.JpaInstrumentQueryRepository;
 import ru.ioque.investfund.adapters.storage.jpa.filter.InstrumentFilterParams;
 import ru.ioque.investfund.application.adapters.DateTimeProvider;
+import ru.ioque.investfund.application.adapters.ExchangeRepository;
 import ru.ioque.investfund.domain.exchange.entity.CurrencyPair;
+import ru.ioque.investfund.domain.exchange.entity.Exchange;
 import ru.ioque.investfund.domain.exchange.entity.Futures;
 import ru.ioque.investfund.domain.exchange.entity.Index;
 import ru.ioque.investfund.domain.exchange.entity.Instrument;
@@ -26,24 +32,116 @@ import ru.ioque.investfund.domain.exchange.value.IndexDeltaResult;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@DisplayName("EXCHANGE REST INTERFACE")
+@DisplayName("EXCHANGE REST CONTROLLER")
 public class ExchangeControllerTest extends BaseControllerTest {
     @Autowired
     JpaInstrumentQueryRepository instrumentQueryRepository;
+    @Autowired
+    ExchangeRepository exchangeRepository;
     @Autowired
     DateTimeProvider dateTimeProvider;
 
     @Test
     @SneakyThrows
     @DisplayName("""
-        T1. Выполнение запроса по эндпоинту GET /api/v1/instruments.
+        T1. Выполнение запроса по эндпоинту POST /api/v1/integrate.
         """)
     public void testCase1() {
+        mvc
+            .perform(MockMvcRequestBuilders.post("/api/v1/integrate"))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    @SneakyThrows
+    @DisplayName("""
+        T2. Выполнение запроса по эндпоинту POST /api/v1/daily-integrate.
+        """)
+    public void testCase2() {
+        mvc
+            .perform(MockMvcRequestBuilders.post("/api/v1/daily-integrate"))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    @SneakyThrows
+    @DisplayName("""
+        T3. Выполнение запроса по эндпоинту POST /api/v1/enable-update.
+        """)
+    public void testCase3() {
+        List<UUID> ids = List.of(UUID.randomUUID(), UUID.randomUUID());
+        mvc
+            .perform(MockMvcRequestBuilders
+                .patch("/api/v1/enable-update")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(new EnableUpdateInstrumentRequest(ids)))
+            )
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    @SneakyThrows
+    @DisplayName("""
+        T4. Выполнение запроса по эндпоинту POST /api/v1/disable-update.
+        """)
+    public void testCase4() {
+        List<UUID> ids = List.of(UUID.randomUUID(), UUID.randomUUID());
+        mvc
+            .perform(MockMvcRequestBuilders
+                .patch("/api/v1/disable-update")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(new DisableUpdateInstrumentRequest(ids)))
+            )
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    @SneakyThrows
+    @DisplayName("""
+        T5. Выполнение запроса по эндпоинту POST /api/v1/disable-update.
+        """)
+    public void testCase5() {
+        final Exchange exchange = Exchange.builder()
+            .id(UUID.randomUUID())
+            .name("EXCHANGE")
+            .url("http://exchange.ru")
+            .description("desc")
+            .instruments(List.of())
+            .updatable(Set.of())
+            .build();
+        Mockito
+            .when(exchangeRepository.get())
+            .thenReturn(exchange);
+        mvc
+            .perform(MockMvcRequestBuilders.get("/api/v1/exchange"))
+            .andExpect(status().isOk())
+            .andExpect(
+                content()
+                    .json(
+                        objectMapper
+                            .writeValueAsString(
+                                ExchangeResponse.builder()
+                                    .name("EXCHANGE")
+                                    .url("http://exchange.ru")
+                                    .description("desc")
+                                    .build()
+                            )
+                    )
+            );
+    }
+
+    @Test
+    @SneakyThrows
+    @DisplayName("""
+        T6. Выполнение запроса по эндпоинту GET /api/v1/instruments.
+        """)
+    public void testCase6() {
         List<Instrument> instrumentInLists = getInstruments();
 
         Mockito
@@ -78,27 +176,25 @@ public class ExchangeControllerTest extends BaseControllerTest {
     @Test
     @SneakyThrows
     @DisplayName("""
-        T2. Выполнение запроса по эндпоинту GET /api/v1/instruments/{id}
+        T7. Выполнение запроса по эндпоинту GET /api/v1/instruments/{id}
         """)
-    public void testCase2() {
-
+    public void testCase7() {
+        LocalDate date = LocalDate.parse("2024-01-12");
         Instrument stock = getInstruments()
             .stream()
             .filter(row -> row.getTicker().equals("TEST_STOCK"))
             .findFirst()
             .orElseThrow();
-        UUID id = stock.getId();
-        LocalDate date = LocalDate.now();
 
         Mockito
             .when(dateTimeProvider.nowDate())
             .thenReturn(date);
         Mockito
-            .when(instrumentQueryRepository.getWithTradingDataBy(id, date))
+            .when(instrumentQueryRepository.getWithTradingDataBy(stock.getId(), date))
             .thenReturn(stock);
 
         mvc
-            .perform(MockMvcRequestBuilders.get("/api/v1/instruments/" + id))
+            .perform(MockMvcRequestBuilders.get("/api/v1/instruments/" + stock.getId()))
             .andExpect(status().isOk())
             .andExpect(
                 content()
