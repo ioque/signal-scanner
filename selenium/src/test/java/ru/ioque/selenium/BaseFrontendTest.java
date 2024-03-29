77602d78cbfe8968b038748c874541274713dcc8
ioque <dev.arven@gmail.com>
2024-03-29 14:49:20 +0600

    chrome options для jenkins

--- selenium/src/test/java/ru/ioque/selenium/BaseFrontendTest.java
+++ selenium/src/test/java/ru/ioque/selenium/BaseFrontendTest.java
@@ -5,10 +5,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.HashMap;
 
 public class BaseFrontendTest {
     protected String uiHost = System.getenv("FRONTEND_HOST");
@@ -17,7 +19,18 @@ public class BaseFrontendTest {
 
     @BeforeEach
     public void beforeEach() {
-        driver = new ChromeDriver();
+        HashMap<String, Object> chromePrefs = new HashMap<>();
+        ChromeOptions options = new ChromeOptions();
+        String downloadFilepath = System.getProperty("downloadFilepath");
+        chromePrefs.put("profile.default_content_settings.popups", 0);
+        chromePrefs.put("download.default_directory", downloadFilepath);
+        options.setExperimentalOption("prefs", chromePrefs);
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920x1080");
+
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
