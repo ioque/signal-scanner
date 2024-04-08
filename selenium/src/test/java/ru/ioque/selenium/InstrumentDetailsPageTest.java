fe08226040a6b0ba967c1ee4cdc2e4080ed23e2a
ioque <dev.arven@gmail.com>
2024-04-08 21:35:00 +0600

    ui тесты, корректировка

--- selenium/src/test/java/ru/ioque/selenium/InstrumentDetailsPageTest.java
+++ selenium/src/test/java/ru/ioque/selenium/InstrumentDetailsPageTest.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -24,44 +23,19 @@ public class InstrumentDetailsPageTest extends BaseFrontendTest {
             .toList()
             .get(0);
         loadPageInstrumentList(datasourceId);
-        var ids = driver
-            .findElement(By.className("table"))
-            .findElements(By.xpath("./child::*"))
-            .get(1)
-            .findElements(By.xpath("./child::*"))
-            .stream().map(row -> row.findElements((By.xpath("./child::*"))).get(0).getText())
-            .toList();
-        verifyDetailsPage(datasourceId, ids.get(0), "BR-1.24 (BRF4)", 128, 1000);
-        verifyDetailsPage(datasourceId, ids.get(1), "USDRUB_TOM (USD000UTSTOM)", 0, 0);
-        verifyDetailsPage(datasourceId, ids.get(2), "Газпромнефть (SIBN)", 123, 1000);
-        verifyDetailsPage(datasourceId, ids.get(3), "Индекс фондового рынка мосбиржи (IMOEX)", 128, 1000);
+        verifyDetailsPage(datasourceId, "BRF4", "BR-1.24 (BRF4)");
+        verifyDetailsPage(datasourceId, "USD000UTSTOM", "USDRUB_TOM (USD000UTSTOM)");
+        verifyDetailsPage(datasourceId, "SIBN", "Газпромнефть (SIBN)");
+        verifyDetailsPage(datasourceId, "IMOEX", "Индекс фондового рынка мосбиржи (IMOEX)");
     }
 
-    private void verifyDetailsPage(String datasourceId, String instrumentId, String name, int dailyValueQnt, int intradayValueQnt) {
+    private void verifyDetailsPage(String datasourceId, String instrumentId, String name) {
         loadPageDetails(datasourceId, instrumentId);
         assertEquals(name, getHeaderElement().getText());
-        assertEquals(dailyValueQnt, getDailyValueRows().size());
-        assertEquals(intradayValueQnt, getIntradayValueRows().size());
     }
 
     private WebElement getHeaderElement() {
         return driver.findElement(By.className("accordion-button"));
-    }
-
-    private List<WebElement> getDailyValueRows() {
-        return driver
-            .findElement(By.id("dailyValueTable"))
-            .findElements(By.xpath("./child::*"))
-            .get(1)
-            .findElements(By.xpath("./child::*"));
-    }
-
-    private List<WebElement> getIntradayValueRows() {
-        return driver
-            .findElement(By.id("intradayValueTable"))
-            .findElements(By.xpath("./child::*"))
-            .get(1)
-            .findElements(By.xpath("./child::*"));
     }
 
     private void loadPageDetails(String datasourceId, String ticker) {
