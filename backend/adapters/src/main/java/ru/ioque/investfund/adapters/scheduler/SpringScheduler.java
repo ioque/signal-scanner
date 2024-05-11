1b5ffa63f3abd6a4eb621b5a023a74337fe64252
ioque <dev.arven@gmail.com>
2024-05-11 11:13:44 +0600

    логирование шуделера

--- backend/adapters/src/main/java/ru/ioque/investfund/adapters/scheduler/SpringScheduler.java
+++ backend/adapters/src/main/java/ru/ioque/investfund/adapters/scheduler/SpringScheduler.java
@@ -3,30 +3,58 @@ package ru.ioque.investfund.adapters.scheduler;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import ru.ioque.investfund.adapters.persistence.repositories.JpaDatasourceRepository;
+import ru.ioque.investfund.application.adapters.CommandPublisher;
+import ru.ioque.investfund.application.adapters.DateTimeProvider;
+import ru.ioque.investfund.application.modules.datasource.command.IntegrateTradingDataCommand;
+import ru.ioque.investfund.application.modules.telegrambot.command.PublishDailyReport;
+import ru.ioque.investfund.application.modules.telegrambot.command.PublishHourlyReport;
+import ru.ioque.investfund.domain.datasource.entity.identity.DatasourceId;
+
+import java.util.concurrent.TimeUnit;
 
 @Component
 @AllArgsConstructor
 @Profile("production")
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class SpringScheduler {
-//    @Scheduled(cron = "0 */15 10-20 * * MON-SUN")
-//    public void integrateTradingData() {
-//        jpaDatasourceRepository.findAll().forEach(datasource -> commandPublisher.publish(
-//            new IntegrateTradingDataCommand(
-//                DatasourceId.from(datasource.getId())
-//            )
-//        ));
-//    }
-//
-//    @Scheduled(cron = "0 0 11-20 * * MON-SUN")
-//    public void publishHourlyReport() {
-//        commandPublisher.publish(new PublishHourlyReport());
-//    }
-//
-//    @Scheduled(cron = "59 59 23 * * MON-SUN")
-//    public void publishDailyReport() {
-//        commandPublisher.publish(new PublishDailyReport());
-//    }
+    Logger log = LoggerFactory.getLogger("SCHEDULE_LOGGER");
+    DateTimeProvider dateTimeProvider;
+    JpaDatasourceRepository jpaDatasourceRepository;
+    CommandPublisher commandPublisher;
+
+    @Scheduled(fixedDelay = 30, timeUnit = TimeUnit.MINUTES)
+    public void healthcheck() {
+        log.info("scheduler is working");
+    }
+
+    @Scheduled(cron = "0 */15 10-20 * * MON-SUN", zone = "Europe/Moscow")
+    public void integrateTradingData() {
+        log.info("start integrate trading data, current time {}", dateTimeProvider.nowDateTime());
+        jpaDatasourceRepository.findAll().forEach(datasource -> commandPublisher.publish(
+            new IntegrateTradingDataCommand(
+                DatasourceId.from(datasource.getId())
+            )
+        ));
+        log.info("finish integrate trading data, current time {}", dateTimeProvider.nowDateTime());
+    }
+
+    @Scheduled(cron = "0 0 11-20 * * MON-SUN", zone = "Europe/Moscow")
+    public void publishHourlyReport() {
+        log.info("start publish hourly report, current time {}", dateTimeProvider.nowDateTime());
+        commandPublisher.publish(new PublishHourlyReport());
+        log.info("finish publish hourly report, current time {}", dateTimeProvider.nowDateTime());
+    }
+
+    @Scheduled(cron = "0 15 20 * * MON-SUN", zone = "Europe/Moscow")
+    public void publishDailyReport() {
+        log.info("start publish daily report, current time {}", dateTimeProvider.nowDateTime());
+        commandPublisher.publish(new PublishDailyReport());
+        log.info("finish publish daily report, current time {}", dateTimeProvider.nowDateTime());
+    }
 }
